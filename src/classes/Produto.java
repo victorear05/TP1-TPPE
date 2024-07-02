81061f9ca1722b5392877f12ade73639eb800edf
Lucas <luk.2001@hotmail.com>
2024-07-02 20:29:32 -0300

    feat: adiciona classe produto e seus testes parametrizados

--- src/classes/Produto.java
+++ src/classes/Produto.java
@@ -1,6 +1,47 @@
 package classes;
 
-
 public class Produto {
+    private Integer codigoItem;
+    private String descricao;
+    private Double valor;
+    private String unidade;
 
+    public Produto(Integer codigoItem, String descricao, Double valor, String unidade) {
+        this.codigoItem = codigoItem;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.unidade = unidade;
+    }
+
+    public Integer getCodigoItem() {
+        return this.codigoItem;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public Double getValor() {
+        return this.valor;
+    }
+
+    public String getUnidade() {
+        return this.unidade;
+    }
+
+    public void setCodigoItem(Integer codigoItem) {
+        this.codigoItem = codigoItem;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
 }
--- src/test/ProdutoTeste.java
+++ src/test/ProdutoTeste.java
@@ -1,10 +1,57 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+import classes.Produto;
 
 @RunWith(Parameterized.class)
 public class ProdutoTeste {
+    private Produto produto;
+    private Integer codigoItem;
+    private String descricao;
+    private Double valor;
+    private String unidade;
 
+    public ProdutoTeste(Produto produto, Integer codigoItem, String descricao, Double valor, String unidade) {
+        this.produto = produto;
+        this.codigoItem = codigoItem;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.unidade = unidade;
+    }
+
+    @Parameters
+    public static Collection<Object[]> getParameters() {
+        return Arrays.asList(new Object[][] {
+                { new Produto(242, "Teclado Mecânico", 109.99, "unidade"), 242, "Teclado Mecânico", 109.99, "unidade" }
+        });
+    }
+
+    @Test
+    public void codigoItemTest() {
+        assertEquals(this.codigoItem, produto.getCodigoItem());
+    }
+
+    @Test
+    public void descricaoTest() {
+        assertEquals(this.descricao, produto.getDescricao());
+    }
+
+    @Test
+    public void valorTest() {
+        assertEquals(this.valor, produto.getValor());
+    }
+
+    @Test
+    public void unidadeTest() {
+        assertEquals(this.unidade, produto.getUnidade());
+    }
 }
